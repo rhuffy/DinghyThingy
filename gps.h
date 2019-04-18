90b307e1cd1ecd14ed4b5dd10c46e0082b73d7a9
Raymond Huffman <rhuffman@mit.edu>
2019-04-18 12:34:59 -0400

    skeleton for peripheral code

---
+++ gps.h
@@ -0,0 +1,6 @@
+#ifndef __GPS_H
+#define __GPS_H
+
+//TODO method headers and constant definitions here.
+
+#endif // __GPS_H
---
+++ sd.h
@@ -0,0 +1,6 @@
+#ifndef __SD_H
+#define __SD_H
+
+//TODO method headers and constant definitions here.
+
+#endif // __SD_H
