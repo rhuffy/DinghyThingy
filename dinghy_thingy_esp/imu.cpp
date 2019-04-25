0102f7da52863a333260ba24654f0c3f87db1d9c
Raymond Huffman <rhuffman@mit.edu>
2019-04-25 10:33:35 -0400

    fix struct syntax

--- dinghy_thingy_esp/imu.cpp
+++ dinghy_thingy_esp/imu.cpp
@@ -18,10 +18,10 @@ void init_imu(){
 IMU_READING_T read_imu(IMU_READING_T* data){
     imu.readAccelData(imu.accelCount);//read imu
     //the unit is in g.
-    x_accel = imu.accelCount[0]*imu.aRes;
-    y_accel = imu.accelCount[1]*imu.aRes;
+    data->x_accel = imu.accelCount[0]*imu.aRes;
+    data->y_accel = imu.accelCount[1]*imu.aRes;
     //there might be a offset for z; not sure if it's just my device
-    z_accel =imu.accelCount[2]*imu.aRes;
+    data->z_accel =imu.accelCount[2]*imu.aRes;
     char output[40];
     //sprintf(output,"%4.2f,%4.2f,%4.2f",x,y,z); //render numbers with %4.2 float formatting
     //Serial.println(output); //print to serial for plotting
